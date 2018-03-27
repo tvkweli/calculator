202b4b3cfc4f9e14e77f1d15b8334e16687b9c15
Talib Visram <talibvisram@Talibs-MacBook-Pro.local>
2018-03-27 12:08:44 -0400

    new

--- main.js
+++ main.js
@@ -4,32 +4,39 @@ $(document).ready(function () {
 			this_row = this_input.closest(".item"),
 			quantity = parseFloat(this_row.find(".qty").val()), // input.val() -> "3" parseFloat("3") -> 3.0
 			// cost = parseFloat(this_row.find(".cost").val()),
-			tariff = parseFloat(this_row.data("tariff-percent")),
-			pre_tariff_cost = (cost * quantity),
-			post_tariff_cost = (pre_tariff_cost + (pre_tariff_cost * (tariff / 100)));
+			grams = parseFloat(this_row.data("milligrams")),
+			totalGrams = (grams * quantity);
+		// post_tariff_cost = (pre_tariff_cost + (pre_tariff_cost * (tariff / 100)));
 
 		// console.log("Quantity", quantity);
 
-		this_row.find(".pre_total").find("span").text(pre_tariff_cost);
-		this_row.find(".post_total").find("span").text(post_tariff_cost);
+		if (isNumber(quantity) && isNumber(grams)) {
+			this_row.find(".total").find("span").text(totalGrams);
+		}
+		// this_row.find(".post_total").find("span").text(post_tariff_cost);
 
-		var pre_total = 0,
-			post_total = 0;
+		var pre_total = 0;
 
-		$(".pre_total").each(function () {
-			//do stuff
-			var this_pre_total = parseFloat($(this).find("span").text());
-			pre_total = pre_total + this_pre_total;
+		$(".calculate").on("click", function (e) {
+			e.preventDefault();
+			$(".total").each(function () {
+				//do stuff
+				var this_total = parseFloat($(this).find("span").text());
+				pre_total = pre_total + this_total;
+			});
+			$("#totalAll").find("span").text(pre_total + "mg");
+			// $(".post_total").each(function () {
+			// 	//more stuff
+			// 	var this_post_total = parseFloat($(this).find("span").text());
+			// 	post_total = post_total + this_post_total;
+			// });
+
 		});
 
-		$(".post_total").each(function () {
-			//more stuff
-			var this_post_total = parseFloat($(this).find("span").text());
-			post_total = post_total + this_post_total;
-		});
-
-		$("#pre_total").find("span").text(pre_total);
-		$("#post_total").find("span").text(post_total);
 
 	});
 });
+
+function isNumber(num) {
+	return !isNaN(parseFloat(num));
+}
